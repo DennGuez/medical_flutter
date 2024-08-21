a1afc63e419595b0d259bb4e5ae922f2f4a7acd7
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2024-08-21 12:18:21 -0400

    Init

---
+++ lib/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        color: Color(0xff51A8FF),
+        height: 350
+      ),
+    );
+  }
+} 
