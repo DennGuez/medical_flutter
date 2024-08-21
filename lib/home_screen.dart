b672f21c75b77a527987c0b8fec77f51e53474e4
Dennis Rodriguez <dennisrodriguezx@gmail.com>
2024-08-21 12:58:25 -0400

    FEAT: TextField added & Fonts

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,7 +10,43 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Container(
         color: Color(0xff51A8FF),
-        height: 350
+/* double.infity hace que el cotainer ocupe todo el espacio */        
+        width: double.infinity,
+        height: 350,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi, Edi',
+              style: TextStyle(
+                fontSize: 18,
+                color: Color(0xffFFFFFF)
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Let's find\nyour top doctor!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+/* Eliminamos el border del Input */                
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none
+                )
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,16 @@ import 'package:medical/home_screen.dart';
 void main() {
   runApp(const MainApp());
 }
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins'
+      ),
       home: HomeScreen(),
     );
   }
